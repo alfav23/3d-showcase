f7f54ca6d368c03d1fe1acefb6c80650224b86b4
Alyssa Favorito <alyssafavorito2000@gmail.com>
2026-01-20 03:01:55 -0500

    create scene

--- main.js
+++ main.js
@@ -6,4 +6,15 @@ const controls = new OrbitControls( camera, renderer.domElement );
 const loader = new GLTFLoader();
 
 const scene = new THREE.Scene();
+
+//Field of view (FOV), aspect ratio, near and far (z-axis render window)
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+function animate() {
+    renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate);
