01b47298afaecb7d0f17877db8bc3aa5ada9008c
Alyssa Favorito <alyssafavorito2000@gmail.com>
2026-01-27 17:59:24 -0500

    updated main imports

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader';
 
 const loader = new GLTFLoader();
 
